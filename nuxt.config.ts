262ef74b0a95af8375e6bc728db7672cc19fcea8
kaee-yori <keysasadepa@gmail.com>
2024-01-16 10:02:55 +0700

    install lucide

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [ '@nuxtjs/tailwindcss' ]
+  devtools: { enabled: true },
+  css: [ '~/assets/css/main.css' ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    'nuxt-lucide-icons'
+  ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="bg-rose-100 min-h-screen">
-        <div class="text-3xl font-semibold text-rose-500"> 
-        ini halaman index
+    <div class="min-h-screen flex">
+        <div class="bg-blue-500 min-h-screen grow grid grid-cols-10">
+            <div class="col-span-10 md:col-span-3 min-h-screen bg-slate-600">
+                <div class="mx-md:fixed">content 1 fixed</div>
+            </div>
+            <div class="col-span-10 md:col-span-7 bg-slate-500">
+                <div class="min-h-screen bg-rose-200">section 1</div>
+                <div class="min-h-screen bg-rose-300">section 2</div>
+                <div class="min-h-screen bg-rose-400">section 3</div>
+                <div class="min-h-screen bg-rose-500">section 4</div>
+            </div>
         </div>
-        <button class="btn btn-neutral">This Is Button</button>
-        <button class="btn btn-primary">This Is Button</button>
-        <button class="btn btn-secondary">This Is Button</button>
-
+        <div class="bg-blue-400 min-h-screen w-28 flex-none mx-md:hidden">
+            <div class="fixed">
+                <div>
+                    <LucideHome :size="30" />
+                    <LucideGithub :size="32" />
+                    <LucideUserRound />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
 </script>
- 
-<style>
+
+<style lang="scss" scoped>
 
 </style>
