2f0c190ed2b5c3766f2ec06bc4192b36cb968aaf
kaee-yori <keysasadepa@gmail.com>
2024-01-16 09:08:23 +0700

    install daisyUI

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="text-3xl font-semibold text-rose-500"> 
-    ini halaman index
+    <div class="bg-rose-100 min-h-screen">
+        <div class="text-3xl font-semibold text-rose-500"> 
+        ini halaman index
+        </div>
+        <button class="btn btn-neutral">This Is Button</button>
+        <button class="btn btn-primary">This Is Button</button>
+        <button class="btn btn-secondary">This Is Button</button>
+
     </div>
 </template>
 
