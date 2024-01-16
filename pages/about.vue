7bfcc4963d2e45ead462633c25c9b024c52e777d
kaee-yori <keysasadepa@gmail.com>
2024-01-16 08:48:46 +0700

    updated tailwind folder

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        ini halaman about
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-3xl font-semibold text-rose-500"> 
+    ini halaman index
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+ 
+<style>
+
+</style>
