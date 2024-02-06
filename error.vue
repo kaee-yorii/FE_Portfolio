ad45a9f091e791b1a595f386449d490640a0abd6
kaee-yori <keysasadepa@gmail.com>
2024-02-06 09:00:25 +0700

    update error vue

--- error.vue
+++ error.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="bg-black w-screen h-screen max-h-screen max-w-screen overflow-hidden relative">
+        <NuxtLink to="/" class="absolute top-10 left-10 text-white text-2xl md:text-4xl font-semibold">{{ url.origin }}
+        </NuxtLink>
+
         <div class="w-full h-full grid grid-rows-2 md:grid-rows-1 items-center px-20">
-            <div class="text-4xl md:text-7xl font-bold text-white mb-2 md:mb-4">ERROR {{ error.statusCode }}</div>
-            <div class="text-3xl md:text-5xl font-bold text-white">{{ error.statusMessage }}</div>
+            <div class="flex flex-col ga-2 md:gap-4">
+                <div class="text-4xl md:text-7xl font-bold text-white mb-2 md:mb-4">ERROR {{ error.statusCode }}</div>
+                <div class="text-3xl md:text-5xl font-bold text-white">
+                    {{ error.statusMessage || error.message || 'Internal Server Error' }}</div>
+                <div class="text-white text-wrap max-w-screen" v-html="error.stack"></div>
+                <NuxtLink to="/" class="btn w-min ">Back To Home</NuxtLink>
+            </div>
         </div>
 
         <IndexImagesBrokenRobot class="w-[110vw] md:w-[80vw] xl:w-[55vw] absolute -right-28 bottom-10" />
@@ -17,5 +25,5 @@ const props = defineProps({
     error: Object
 })
 
-console.log(props.error)
+const url = useRequestURL();
 </script>
