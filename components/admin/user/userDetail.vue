b15229ec5bd096388ecd95717b91033f0f2ed5d3
kaee-yori <keysasadepa@gmail.com>
2024-02-20 11:13:57 +0700

    fixing click button

--- components/admin/user/userDetail.vue
+++ components/admin/user/userDetail.vue
@@ -26,30 +26,9 @@
         </label>
 
     </div>
-    <label class="btn grow mt-3 w-[320px]" for="confirm">Submit</label>
+    <label class="btn grow mt-3 w-[320px]" @click="confirm = true">Submit</label>
     <div class="text-xs text-error" v-if="fetchError">{{ fetchError }}</div>
-    <AdminUserModalConfirm :show="confirm" @close="false" />
-    <AdminModalSuccess />
-
-
-    <!-- modal -->
-    <input type="checkbox" id="confirm" class="modal-toggle">
-    <div class="modal" role="dialog">
-        <div class="modal-box">
-            <form method="dialog">
-                <label for="confrim" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">x</label>
-            </form>
-        </div>
-        <h3 class="font-bold text-lg">Confirm To Processed</h3>
-        <p class="py-4">Are You Sure?</p>
-        <div class="modal-action">
-            <label for="confirm" class="btn text-white btn-error">Cancel</label>
-            <label for="confirm" class="btn text-white btn-success" @click="handleUpdate">Yes Update!</label>
-        </div>
-    </div>
-    <form method="dialog" class="modal-backdrop">
-        <label for="confirm">Close</label>
-    </form>
+    <AdminUserModalConfirm :show="confirm" @close="confirm = false" />
 </template>
 
 <script setup>
@@ -58,7 +37,6 @@ const AuthStore = useAuthStore();
 
 const errors = ref({});
 const fetchError = ref('');
-const confirm = ref(false)
 
 const formData = ref({
     name: AuthStore.user.name,
@@ -68,16 +46,14 @@ const formData = ref({
     confirm_password: ''
 });
 
+const confirm = ref(false);
 const success = ref(false);
 const handleUpdate = async () => {
-    // reset errors
     errors.value = {}
     fetchError.value = '';
     success.value = false;
-    confirm = false;
 
     try {
-
         console.log('masuk handle update')
         await AuthStore.updateUser(formData.value);
         success.value = true;
@@ -85,7 +61,6 @@ const handleUpdate = async () => {
     } catch (error) {
         console.log('ada error')
         console.log(error)
-
         if (error instanceof Joi.ValidationError) {
             // joi error
             errors.value = joiError(error);
@@ -95,5 +70,5 @@ const handleUpdate = async () => {
             fetchError.value = error.data.message;
         }
     }
-};
+}
 </script>
